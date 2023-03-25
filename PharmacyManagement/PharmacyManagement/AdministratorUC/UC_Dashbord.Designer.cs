ee7a484c75d4b0afef6430466400e34fd4188d97
ibrahimuyumaz <ibrahimuyumaz001@gmail.com>
2023-03-25 21:46:52 +0300

    administrator

--- PharmacyManagement/PharmacyManagement/AdministratorUC/UC_Dashbord.Designer.cs
+++ PharmacyManagement/PharmacyManagement/AdministratorUC/UC_Dashbord.Designer.cs
@@ -34,6 +34,13 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -53,26 +60,70 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label2);
             this.panel1.Location = new System.Drawing.Point(54, 111);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(308, 597);
+            this.panel1.Size = new System.Drawing.Size(308, 623);
             this.panel1.TabIndex = 1;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel2.Controls.Add(this.label3);
             this.panel2.Location = new System.Drawing.Point(401, 111);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(308, 597);
+            this.panel2.Size = new System.Drawing.Size(308, 623);
             this.panel2.TabIndex = 2;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel3.Controls.Add(this.label4);
             this.panel3.Location = new System.Drawing.Point(750, 111);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(308, 597);
+            this.panel3.Size = new System.Drawing.Size(308, 623);
             this.panel3.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(76, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Administrator";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(95, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Pharmacist";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(101, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Customer";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(33, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(181, 100);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Task Performed\r\n\r\n      *Add User\r\n      *View User\r\n      *Profile Management";
             // 
             // UC_Dashbord
             // 
@@ -84,7 +135,13 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Name = "UC_Dashbord";
-            this.Size = new System.Drawing.Size(1105, 766);
+            this.Size = new System.Drawing.Size(1105, 792);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,5 +154,9 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
     }
 }
